4b0da0ca67a6589785fd5c6be0519887710f31fb
Jack Waddington <jack@waddington.me>
2024-11-17 22:20:05 +0200

    some files

---
+++ router_configs/get_configs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Replace with the IP address of the remote host
+REMOTE_HOST="192.168.0.215"
+
+# List of files to copy
+FILES=("../../etc/dhcpd.conf" "../../etc/hostname.bse0" "../../etc/hostname.bwfm0" "../../etc/hostname.ure0" "pf.conf")
+
+# Use SCP to copy files
+for file in "${FILES[@]}"; do
+  scp $REMOTE_HOST:$file .
+done
